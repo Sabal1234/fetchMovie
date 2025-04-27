fee025d910c0571b1d9cedb02681a37af10b6541
Sabal Adhikari <sadaladhikari@gmail.com>
2025-04-27 20:50:00 +0545

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { GetMovie } from './component/GetMovie'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
